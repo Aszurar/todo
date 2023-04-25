a6030d56cec8142e091de258011516d00a02e944
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-24 21:40:26 -0300

    :lipstick: Adicionando arquivo de tema  :lipstick:

---
+++ src/theme/index.ts
@@ -0,0 +1,30 @@
+const THEME = {
+  COLORS: {
+    PURPLE: '#7159C1',
+    PURPLE_DARK: '#5A4CA3',
+    PURPLE_TRANSLUCENT: '#7159C133',
+
+    BLUE: '#4EA8DE',
+    BLUE_DARK: '#1E6F9F',
+
+    DANGER: '#E25858',
+
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+
+    GRAY_100: '#F2F2F2',
+    GRAY_200: '#D9D9D9',
+    GRAY_300: '#808080',
+    GRAY_400: '#333333',
+    GRAY_500: '#262626',
+    GRAY_600: '#191919',
+    GRAY_700: '#1A1A1A',
+    GRAY_800: '#0D0D0D',
+  },
+  FONTS: {
+    REGULAR: 'Inter_400Regular',
+    BOLD: 'Inter_700Bold',
+  },
+};
+
+export default THEME;
