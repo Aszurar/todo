68f3da6d5092f2c5c0b31a00543edd00bf33f886
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-28 21:08:11 -0300

    :clown_face: Mock de dados para facilitar o desenvolvimento e tipagem das tarefas  :clown_face:

---
+++ src/dto/todoDTO.ts
@@ -0,0 +1,88 @@
+export type TodoProps = {
+  id: string;
+  title: string;
+  isChecked: boolean;
+};
+
+const MAX_TOTO_TITLE_LENGTH = 250;
+
+export const TODO_DEFAULT = [
+  {
+    id: '1',
+    title: 'Fazer café',
+    isChecked: false,
+  },
+  {
+    id: '2',
+    title: 'Estudar React Native',
+    isChecked: false,
+  },
+  {
+    id: '3',
+    title: 'Estudar React',
+    isChecked: false,
+  },
+  {
+    id: '4',
+    title: 'Estudar Node',
+    isChecked: false,
+  },
+  {
+    id: '5',
+    title: 'Estudar Typescript',
+    isChecked: false,
+  },
+  {
+    id: '6',
+    title: 'Estudar Javascript',
+    isChecked: false,
+  },
+  {
+    id: '7',
+    title: 'Estudar HTML',
+    isChecked: false,
+  },
+  {
+    id: '8',
+    title: 'Estudar CSS',
+    isChecked: false,
+  },
+  {
+    id: '9',
+    title: 'Estudar C#',
+    isChecked: false,
+  },
+  {
+    id: '10',
+    title: 'Estudar Java',
+    isChecked: false,
+  },
+  {
+    id: '11',
+    title: 'Estudar Python',
+    isChecked: false,
+  },
+  {
+    id: '12',
+    title: 'Estudar PHP',
+    isChecked: false,
+  },
+  {
+    id: '13',
+
+    title: 'Estudar C++',
+    isChecked: false,
+  },
+  {
+    id: '14',
+    title: 'Estudar C',
+    isChecked: false,
+  },
+  {
+    id: '15',
+    title: 'Estudar Ruby',
+    isChecked: false,
+  },
+];
+
+export { MAX_TOTO_TITLE_LENGTH };
