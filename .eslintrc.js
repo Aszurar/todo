5eca6e07867c7486b0764f55174f9d68cf8b413a
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-05-11 12:00:03 -0300

    :art: Adicionando arquivo de formatação :art:

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,19 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
-        'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+        'sort-imports': [
+          'error',
+          { ignoreCase: true, ignoreDeclarationSort: true },
+        ],
         'import/order': [
           'error',
           {
-            groups: [['external', 'builtin'], 'internal', ['sibling', 'parent'], 'index'],
+            groups: [
+              ['external', 'builtin'],
+              'internal',
+              ['sibling', 'parent'],
+              'index',
+            ],
             pathGroups: [
               {
                 pattern: 'react',
@@ -22,12 +30,21 @@ module.exports = {
                 position: 'before',
               },
               {
-                pattern: 'native-base',
+                pattern:
+                  '{react-native,expo*,expo,@expo/**,expo-*,@expo-*,@expo-**,@expo-google-fonts/**,@expo/*,expo/*}',
                 group: 'external',
                 position: 'before',
               },
               {
-                pattern: '{react*,react-*,@react*,@react-*,@react*/**,@react-*/**,expo*,styled-components/**,}',
+                pattern:
+                  '{native-base,styled-components,styled-components/native,styled-components/primitives,styled-components/**}',
+                group: 'external',
+                position: 'before',
+              },
+
+              {
+                pattern:
+                  '{react*,react-*,@react*,@react-*,@react*/**,@react-*/**,styled-components/**,}',
                 group: 'external',
                 position: 'before',
               },
@@ -37,7 +54,8 @@ module.exports = {
                 position: 'before',
               },
               {
-                pattern: '{@icons/**,@images/**,assets/**,../assets/**,../../assets/**}',
+                pattern:
+                  '{@icons/**,@images/**,assets/**,../assets/**,../../assets/**}',
                 group: 'internal',
                 position: 'before',
               },
@@ -48,22 +66,26 @@ module.exports = {
                 position: 'before',
               },
               {
-                pattern: '{@atoms/**,@molecules/**,@organisms/**,components/**,../components/**,../../components/**}',
+                pattern:
+                  '{@atoms/**,@molecules/**,@organisms/**,components/**,../components/**,../../components/**}',
                 group: 'internal',
                 position: 'before',
               },
               {
-                pattern: '{@styles/**,@utils/**,utils/**,../utils/**,../../utils/**}',
+                pattern:
+                  '{@styles/**,@utils/**,utils/**,../utils/**,../../utils/**}',
                 group: 'internal',
                 position: 'before',
               },
               {
-                pattern: '{@dtos/**,@translations/**,@dto/**,dto/**,../dto/**,../../dto/**}',
+                pattern:
+                  '{@dtos/**,@translations/**,@dto/**,dto/**,../dto/**,../../dto/**}',
                 group: 'internal',
                 position: 'before',
               },
               {
-                pattern: './styles,@theme/**,theme/**,../theme/**,../../theme/**',
+                pattern:
+                  '{./styles,@theme/**,theme/**,../theme/**,../../theme/**}',
                 group: 'internal',
                 position: 'before',
               },
